78e13ddc8bed50ecdf81cdf112f13cac563335f5
WedithaV <wedithav@gmail.com>
2025-10-27 01:24:13 +0530

    create CheckoutPopup component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import LandingPage from '@/components/layouts/LandingPage';
+import CheckoutPopup from '@/components/UI/CheckoutPopup';
 
 export default function Home() {
   return (
     <div>
-      <LandingPage />
+      <CheckoutPopup />
     </div>
   );
 }
