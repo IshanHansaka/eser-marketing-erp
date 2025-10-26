f93885f61d90c43ae3d6eb4fd07716fdcb01aec6
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-10-26 11:38:08 +0530

    feat: implement  layout and page structure

---
+++ src/app/Customer/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomerHomePage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Customer Dashboard</h1>
+    </div>
+  );
+}
---
+++ src/app/Employee/page.tsx
@@ -0,0 +1,7 @@
+export default function EmployeeHomePage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Employee Dashboard</h1>
+    </div>
+  );
+}
