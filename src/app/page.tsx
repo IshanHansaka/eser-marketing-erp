4a0f7d7785545248d10ed2e2463fd311b407093d
WedithaV <wedithav@gmail.com>
2025-10-27 01:28:34 +0530

    Replace CheckoutPopup with LandingPage in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import LandingPage from '@/components/layouts/LandingPage';
-import CheckoutPopup from '@/components/UI/CheckoutPopup';
 
 export default function Home() {
   return (
     <div>
-      <CheckoutPopup />
+      <LandingPage />
     </div>
   );
 }
