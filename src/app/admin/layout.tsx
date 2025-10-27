52b6435a9699ff4963b7f79681a6d554641998ae
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-10-27 09:44:43 +0530

    fix: remove unnessary code from css

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,9 +11,10 @@ export default function AdminLayout({
     <div className="flex">
       <AdminSidebar />
       <div className="flex-1 bg-white min-h-screen">
-      <div>
-        <AdminHeader />
-        <div className="flex-1 p-6 ml-60">{children}</div>
+        <div>
+          <AdminHeader />
+          <div className="flex-1 p-6 ml-60">{children}</div>
+        </div>
       </div>
     </div>
   );
