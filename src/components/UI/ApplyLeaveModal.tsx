8c65d03e44bf7b8b67563ab014cd9010a8fc443c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-10-27 09:42:30 +0530

    fix: remove unnessary code from css

--- src/components/UI/ApplyLeaveModal.tsx
+++ src/components/UI/ApplyLeaveModal.tsx
@@ -91,7 +91,7 @@ export default function ApplyLeaveModal({
                     name="fromDate"
                     value={formData.fromDate}
                     onChange={handleInputChange}
-                    className="w-full pl-10 </button>pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-600"
+                    className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-600"
                     placeholder="Select date"
                     required
                   />
