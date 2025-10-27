649092074ffd116ea9999cf70861c3868c2f9e3a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-10-27 15:09:03 +0530

    chore: add style chnages

--- src/app/customer/layout.tsx
+++ src/app/customer/layout.tsx
@@ -9,7 +9,7 @@ export default function EmployeeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
+    <div className="flex-1 bg-gray-50 min-h-screen">
       <CustomerSidebar />
       <div>
         <CustomerHeader />
